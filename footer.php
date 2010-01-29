abd4f5d49035b2514bc4a419314b630e48ae189d
masteryoer <masteryoer@4bfd2c98-05b6-11df-a276-edb32ab3677a>
2010-01-29 12:07:10 +0000

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+echo '<div id="blank">&nbsp;</div>
+
+<table width="650" border="0" cellspacing="0" cellpadding="0" class="surround">
+  <tr>
+    <td align="center">Page generated in 0.0003 seconds<br />      
+      TrinityManager v0.1<br /></td>
+  </tr>
+</table>
+</center>		
+</body>
+</html>';
+?>
